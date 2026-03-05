d5242fba227386399a9dff44795ebddfee38d8ef
uo287935 <uo287935@uniovi.es>
2026-03-05 17:12:33 +0100

    nuevo cambios 2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // CSS global
+import './index.css'; // CSS global!
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
